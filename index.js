adad4a963971e14cdeeaa89f3484bc802c08a4e3
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-09 10:39:36 +0300

    commit tree ed973196bebd43641b75a3fcaf41174aebd1fd94

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(arrayOfDrivers, stringToMatch)
+{   
+    const newArrayOfDrivers = arrayOfDrivers.filter(x => x.toUpperCase() === stringToMatch.toUpperCase());
+
+    return newArrayOfDrivers;
+}
+
+//const names = ["sally", "sammy", "bobby"];
+//debugger;
+//const letters = "sa"
+
+function fuzzyMatch(arrayOfDrivers, stringOfLetters){
+    const newArray = arrayOfDrivers.filter(x => x[0] + x[1] === stringOfLetters);
+    return newArray;
+    }
+
+function matchName(arrayOfObjects, stringToMatch){
+    //look through array for name object properties
+    const newArray = arrayOfObjects.filter(x => x["name"] === stringToMatch);
+    return newArray;
+    
+}
